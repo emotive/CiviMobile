36456e2d3a81c634eb42687f29c29f9e8f4b8162
mbriney <matthew@emotivellc.com>
2010-06-09 22:23:18 -0700

    Added tabs

---
+++ CiviMobile/Resources/login.js
@@ -0,0 +1,76 @@
+//Add scrolling form window with logo
+
+var scrollView = Titanium.UI.createScrollView({
+    contentWidth:'auto',
+    contentHeight:'auto',
+    top:0,
+    showVerticalScrollIndicator:false,
+    showHorizontalScrollIndicator:false
+});
+var loginform = Ti.UI.createView({
+    width:'auto',
+    height:2000,
+    top:200
+});
+
+
+scrollView.add(loginform);
+background.add(scrollView);
+
+var logo = Titanium.UI.createImageView({
+url:'images/civi-logo.png',
+top:10,
+width:194,
+height:189
+});
+
+scrollView.add(logo);
+
+
+// Create login form
+
+
+var loginuser = Titanium.UI.createTextField({
+	value:'username',
+	height:35,
+	width:238,
+	top:225,
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+var loginpass = Titanium.UI.createTextField({
+	value:'password',
+	height:35,
+	width:238,
+	top:275,
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+var loginurl = Titanium.UI.createTextField({
+	value:'url',
+	height:35,
+	width:238,
+	top:325,
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+var loginbutton = Titanium.UI.createButton({
+    title: 'Login',
+	height:43,
+	width:238,
+	top:375,
+	backgroundImage:'images/button_green.png',
+    color:'#fff',
+    font:{fontWeight:'bold',fontSize:20}
+
+});
+loginbutton.addEventListener('click',function(e)
+{
+   Titanium.API.info("You clicked the button");
+});
+
+
+scrollView.add(loginuser);
+scrollView.add(loginpass);
+scrollView.add(loginurl);
+scrollView.add(loginbutton);
---
+++ CiviMobile/Resources/tabs.js
@@ -0,0 +1,100 @@
+// create tab group
+var tabGroup = Titanium.UI.createTabGroup();
+
+
+//Create windows for tabs
+
+var window_summary = Titanium.UI.createWindow({  
+    title:'Database Snapshot'
+});
+var window_search = Titanium.UI.createWindow({  
+    title:'Search'
+});
+var window_nearby = Titanium.UI.createWindow({  
+    title:'Who is Nearby?'
+});
+var window_reports = Titanium.UI.createWindow({  
+    title:'Reports'
+});
+
+
+// Create tabs
+
+var tab_summary = Titanium.UI.createTab({  
+    icon:'images/tab-summary.png',
+    title:'Snapshot',
+    window:window_summary
+});
+
+var tab_search = Titanium.UI.createTab({  
+    icon:'images/tab-search.png',
+    title:'Search',
+    window:window_search
+});
+
+var tab_nearby = Titanium.UI.createTab({  
+    icon:'images/tab-nearby.png',
+    title:'Nearby',
+    window:window_nearby
+});
+
+var tab_reports = Titanium.UI.createTab({  
+    icon:'images/tab-reports.png',
+    title:'Reports',
+    window:window_reports
+});
+
+
+tabGroup.addTab(tab_summary);  
+tabGroup.addTab(tab_search);  
+tabGroup.addTab(tab_nearby);  
+tabGroup.addTab(tab_reports);  
+
+
+// Add content
+
+var label1 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'I am Window 1',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+window_summary.add(label1);
+
+var label2 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'I am Window 2',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+window_search.add(label2);
+
+var label3 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'I am Window 3',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+window_nearby.add(label3);
+
+var label4 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'I am Window 4',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+window_reports.add(label4);
+
+
+
+
+// open tab group
+tabGroup.open();
